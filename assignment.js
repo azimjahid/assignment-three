fa3a6d151e90813c0f0d3b6563919dd0bdbe75af
Azim Ahmed <azimjahid@gmail.com>
2021-01-22 21:05:17 +0600

    updated

--- assignment.js
+++ assignment.js
@@ -6,25 +6,24 @@ function kilometerToMeter(kilometer) {
 }
 var ctgToDhaka = kilometerToMeter(220);
 console.log(ctgToDhaka);
-  // Problem one end
-  
-  // Problem Two: budgetCalculator
-  // Problem two start
- function budgetCalculator(watch, phone, laptop){
-    var watch = watch* 50;
-    var phone = phone* 100;
-    var laptop = laptop* 500;
-    var total = watch+phone+laptop;
-    return total
-  }       
-  var totalBudget =budgetCalculator (7, 5, 3);
-  console.log(totalBudget);
-  // Problem two end
-  
-  // Problem Three: hotelCost
-   // Problem three start
-  function hotelCost(day) {
+// Problem one end
 
+// Problem Two: budgetCalculator
+// Problem two start
+function budgetCalculator(watch, phone, laptop) {
+    var watch = watch * 50;
+    var phone = phone * 100;
+    var laptop = laptop * 500;
+    var total = watch + phone + laptop;
+    return total
+}
+var totalBudget = budgetCalculator(7, 5, 3);
+console.log(totalBudget);
+// Problem two end
+
+// Problem Three: hotelCost
+// Problem three start
+function hotelCost(day) {
     var cost = 0;
     if (day <= 10) {
         cost = day * 100;
@@ -33,8 +32,7 @@ console.log(ctgToDhaka);
         var remainingDays = day - 10;
         var secondTenDays = remainingDays * 80;
         cost = firstTenDays + secondTenDays;
-
-
+        
     } else {
         var firstTenDays = 10 * 100;
         var secondTenDays = 10 * 80;
@@ -46,11 +44,11 @@ console.log(ctgToDhaka);
 }
 var totalCost = hotelCost(28);
 console.log(totalCost);
-    // Problem three end
-  
-  // Problem 4:megaFriend
-   // Problem four start
-  function megaFriend(friends) {
+// Problem three end
+
+// Problem 4:megaFriend
+// Problem four start
+function megaFriend(friends) {
     if (friends.length > 0) {
         var richFriend = friends[0];
         for (var i = 0; i < friends.length; i++) {
@@ -69,4 +67,4 @@ console.log(totalCost);
 var friends = ["Azim Ahmed", "Azim Jahid", "Regan Ahmed"];
 var ModernFriend = megaFriend(friends);
 console.log(ModernFriend);
- // Problem four end
+// Problem four end
